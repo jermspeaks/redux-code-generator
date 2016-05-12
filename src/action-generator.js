7ea7df2246b22e2dcf8699eff7fb6445c131aac1
jermspeaks <jermspeaks@gmail.com>
2016-05-12 16:17:19 -0700

    action generator created

---
+++ src/action-generator.js
@@ -0,0 +1,76 @@
+/**
+ * Create request function for an action
+ * @param  {object} settings Action settings
+ * @return {string}          Request function generated
+ */
+function createRequestFunction(settings) {
+  return `
+/**
+ * Action generator for ${settings.name} request action
+ * @return {object}         request action
+ */
+function ${settings['method_base']}RequestAction() {
+  return {
+    type: ${settings['constant_name']}_REQUEST
+  }
+}`;
+}
+
+/**
+ * Create success function for an action
+ * @param  {object} settings Action settings
+ * @return {string}          Success function generated
+ */
+function createSuccessFunction(settings) {
+  return `
+/**
+ * Action generator for ${settings.name} success action
+ * @param  {object} data    API response
+ * @return {object}         success action
+ */
+function ${settings['method_base']}SuccessAction(data) {
+  return {
+    type: ${settings['constant_name']}_SUCCESS,
+    data: data
+  }
+}`;
+}
+
+/**
+ * Create failure function for an action
+ * @param  {object} settings Action settings
+ * @return {string}          Failure function generated
+ */
+function createFailureFunction(settings) {
+  return `
+/**
+ * Action generator for ${settings.name} failure action
+ * @param  {string|object} error    error from API
+ * @return {object}                 failure action
+ */
+function ${settings['method_base']}FailureAction(error) {
+  return {
+    type: ${settings['constant_name']}_FAILURE,
+    error: error
+  }
+}`;
+}
+
+const settings = {
+  name: 'sample one',
+  method_base: 'sampleOne',
+  constant_name: 'SAMPLE_ONE',
+  grouped_method: 'getSampleOneData',
+  fetch_api_method: 'fetchSampleOneData',
+  api_method: 'getSampleOne'
+};
+
+
+const failureFunction = createFailureFunction(settings);
+console.log(failureFunction);
+
+module.exports = {
+  createRequestFunction: createRequestFunction,
+  createSuccessFunction: createSuccessFunction,
+  createFailureFunction: createFailureFunction
+};
---
+++ tests/action-generator.spec.js
@@ -0,0 +1,67 @@
+const expect = require('expect');
+const generator = require('../src/action-generator');
+
+describe('action generators', () => {
+  const settings = {
+    name: 'sample one',
+    method_base: 'sampleOne',
+    constant_name: 'SAMPLE_ONE',
+    grouped_method: 'getSampleOneData',
+    fetch_api_method: 'fetchSampleOneData',
+    api_method: 'getSampleOne'
+  };
+
+  describe('createRequestFunction', () => {
+    it('generates a proper request function', () => {
+      var generatedString = `
+/**
+ * Action generator for sample one request action
+ * @return {object}         request action
+ */
+function sampleOneRequestAction() {
+  return {
+    type: SAMPLE_ONE_REQUEST
+  }
+}`;
+      expect(generator.createRequestFunction(settings)).toEqual(generatedString);
+    });
+
+  });
+
+  describe('createSuccessFunction', () => {
+    it('generates a proper success function', () => {
+      var generatedString = `
+/**
+ * Action generator for sample one success action
+ * @param  {object} data    API response
+ * @return {object}         success action
+ */
+function sampleOneSuccessAction(data) {
+  return {
+    type: SAMPLE_ONE_SUCCESS,
+    data: data
+  }
+}`;
+      expect(generator.createSuccessFunction(settings)).toEqual(generatedString);
+    });
+  });
+
+  describe('createFailureFunction', () => {
+    it('generates a proper failure function', () => {
+      var generatedString = `
+/**
+ * Action generator for sample one failure action
+ * @param  {string|object} error    error from API
+ * @return {object}                 failure action
+ */
+function sampleOneFailureAction(error) {
+  return {
+    type: SAMPLE_ONE_FAILURE,
+    error: error
+  }
+}`;
+      expect(generator.createFailureFunction(settings)).toEqual(generatedString);
+    });
+  });
+
+})
