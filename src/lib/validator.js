1d02a571aa52d13a3d8618002b046071517972af
Jeremy <jermspeaks@gmail.com>
2016-05-13 21:13:03 -0700

    good portion of validation tests written

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -8,7 +8,9 @@ const validations = require('./validations');
 function validateSettings(settings) {
   validations.validSettings(settings) ? null : exitProcess('YAML settings not valid');
   validations.validSettingsType(settings) ? null : exitProcess('YAML settings not properly converted to an object');
-  validations.validSettingsKeys(settings) ? null : exitProcess('Forgot Actions or Reducer key');
+  validations.validSettingsKeys(settings) ? null : exitProcess('Invalid keys. Must include Actions, Output, and Reducer');
+  validations.validActionSettingsType(settings.actions) ? null : exitProcess('Invalid actions. Must be array');
+  settings.actions.forEach(action => validations.validActionSettingsKeys(action) ? null : exitProcess('Invalid action settings for ${action.name}. Must include name and reducer'));
 };
 
 function exitProcess(error) {
---
+++ tests/lib/validations.spec.js
@@ -0,0 +1,115 @@
+const expect = require('expect');
+const validations = require('../../src/lib/validations');
+
+describe('YAML settings validations', () => {
+  const falsyValues = [
+    false,
+    null,
+    undefined,
+    ''
+  ];
+
+  falsyValues.forEach(setting => {
+    it(`invalidates settings with falsy value: ${setting}`, () => {
+      expect(validations.validSettings(setting)).toBe(false);
+    });
+  });
+
+  it('validates settings with a truthy value', () => {
+    expect(validations.validSettings(true)).toBe(true);
+  });
+
+  const nonObjectSettings = [
+    'string',
+    3
+  ];
+
+  nonObjectSettings.forEach(setting => {
+    it(`invalidates settings when a ${typeof setting} is not an object`, () => {
+      expect(validations.validSettingsType(setting)).toBe(false);
+    });
+  });
+
+  it('validates settings when an object is an object', () => {
+    expect(validations.validSettingsType({})).toBe(true);
+  });
+
+  const mainKeys = [{
+    name: 'reducer',
+    settings: {
+      actions: {},
+      output: {}
+    }
+  }, {
+    name: 'output',
+    settings: {
+      actions: {},
+      reducer: {}
+    }
+  }, {
+    name: 'actions',
+    settings: {
+      output: {},
+      reducer: {}
+    }
+  }];
+
+  mainKeys.forEach(mainKey => {
+    it(`invalidates settings for missing ${mainKey.name} key`, () => {
+      expect(validations.validSettingsKeys(mainKey.settings)).toBe(false);
+    });
+  });
+
+  it('validates settings for having all valid keys', () => {
+    const settings = {
+      actions: {},
+      output: {},
+      reducer: {}
+    };
+
+    expect(validations.validSettingsKeys(settings)).toBe(true);
+  });
+
+  const nonArrayActionSettings = [
+    'string',
+    3,
+    { foo: 'bar' }
+  ];
+
+  nonArrayActionSettings.forEach(action => {
+    it(`invalidates action settings as an Array for ${action.toString()}`, () => {
+      expect(validations.validActionSettingsType(action)).toBe(false);
+    });
+  });
+
+  it('validates action settings as an Array', () => {
+    expect(validations.validActionSettingsType([])).toBe(true);
+  });
+
+  const actionKeys = [{
+    name: 'reducer',
+    settings: {
+      name: 'Valid Name',
+    }
+  }, {
+    name: 'name',
+    settings: {
+      reducer: {},
+    }
+  }];
+
+  actionKeys.forEach(actionKey => {
+    it(`invalidates action settings for missing ${actionKey.name} key`, () => {
+      expect(validations.validActionSettingsKeys(actionKey.settings)).toBe(false);
+    });
+  });
+
+  it(`validates action settings`, () => {
+    const actionSettings = {
+      name: 'Valid Name',
+      reducer: {}
+    };
+
+    expect(validations.validActionSettingsKeys(actionSettings)).toBe(true);
+  });
+});
